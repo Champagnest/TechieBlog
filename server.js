6e6e750a9a8db6e4cd0b7c59efda049dfd3f8b51
Champagnest <mchampagned@protonmail.com>
2022-05-21 19:46:57 -0700

    got the server working, need posts to post

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const sess = {
     db: sequelize,
   }),
 };
-// app.use(sesson(sess));
+app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
